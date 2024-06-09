5ece35cad4c85ff1e6358258d6b5a0204c75da3e
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-06-09 14:16:03 +0530

    Admin authentication middleware done

--- apps/server/src/loaders/routes.ts
+++ apps/server/src/loaders/routes.ts
@@ -2,11 +2,12 @@ import { Express } from 'express';
 import authRouter from '../routes/auth.route';
 import roleRouter from '../routes/role.route';
 import userRouter from '../routes/user.route';
+import { isAdmin } from '../middlewares';
 
 const routesLoader = (app: Express) => {
   app.use('/auth', authRouter);
-  app.use('/role', roleRouter);
-  app.use('/user', userRouter);
+  app.use('/role', isAdmin, roleRouter);
+  app.use('/user', isAdmin, userRouter);
 };
 
 export default routesLoader;
--- apps/server/src/middlewares/authentication.ts
+++ apps/server/src/middlewares/authentication.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import passport from 'passport';
 
+import { eq } from 'drizzle-orm';
 import { BadRequestError, UnauthenticatedError } from '../errors';
+import { db } from '../models';
+import { RoleSchema, UserSchema } from '../models/schema';
 
 export const authenticate = passport.authenticate('local');
 
@@ -29,12 +32,32 @@ export const isAuthenticated = (
   }
 };
 
-export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
-  if (
-    req.isAuthenticated() /** && Some condition to check if the user is admin */
-  ) {
-    next();
+export const isAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.isAuthenticated()) {
+    const userWithRole = await db.query.UserSchema.findFirst({
+      where: eq(UserSchema.id, req.user.id),
+      with: {
+        userToRole: {
+          columns: {
+            role_id: true
+          }
+        }
+      }
+    });
+    userWithRole?.userToRole.forEach(async ({ role_id }) => {
+      const role = await db.query.RoleSchema.findFirst({
+        where: eq(RoleSchema.id, role_id)
+      });
+      if (role?.name === 'admin') {
+        return next();
+      }
+    });
+    throw new UnauthenticatedError('You are not an Admin!');
   } else {
-    throw new UnauthenticatedError('You are not authenticated!');
+    throw new UnauthenticatedError('You are not an Admin!');
   }
 };
