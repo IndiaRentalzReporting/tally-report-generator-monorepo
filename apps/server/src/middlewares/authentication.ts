8062a732bff39b8e53a00864ea2aed1ba6ea1a9c
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-06-12 15:06:01 +0530

    minor change

--- apps/server/src/middlewares/authentication.ts
+++ apps/server/src/middlewares/authentication.ts
@@ -36,7 +36,7 @@ export const isAdmin = async (
 ) => {
   if (req.isAuthenticated()) {
     const user = await UserService.findOne({ email: req.user.email });
-    if (user?.role?.name === 'admin') {
+    if (user?.role?.name.toLowerCase() === 'admin') {
       return next();
     }
   }
