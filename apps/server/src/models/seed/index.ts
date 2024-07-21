28f88e4eb3d489216aeccb064fa8ce55b03b3439
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-07-21 22:09:03 +0530

    seeding script edits

--- apps/server/src/models/seed/index.ts
+++ apps/server/src/models/seed/index.ts
@@ -1,9 +1,11 @@
+import { ModuleColumnsValue } from '@fullstack_package/interfaces';
 import config from '../../config';
 import ActionService from '../../services/ActionService';
 import AuthService from '../../services/AuthService';
 import ModuleService from '../../services/ModuleService';
 import RoleService from '../../services/RoleService';
 import UserService from '../../services/UserService';
+import { ModuleInsert } from '../schema';
 
 const createActions = async () => {
   const actions = ['CREATE', 'READ', 'UPDATE', 'DELETE'].map(async (name) => {
@@ -14,8 +16,49 @@ const createActions = async () => {
 };
 
 const createModules = async () => {
-  const modules = [{ name: 'ROLES', columns: [{}] }, 'USERS', 'MODULES'].map(
-    (module) => ModuleService.createOne({ name: module })
+  const modules: Array<{
+    moduleDetails: ModuleInsert;
+    columnDetails: Array<{
+      name: ModuleInsert['name'];
+      type: ModuleColumnsValue;
+    }>;
+  }> = [
+    {
+      moduleDetails: { name: 'ROLES' },
+      columnDetails: [
+        { name: 'id', type: 'UUID' },
+        { name: 'name', type: 'VARCHAR(50)' },
+        { name: 'createdAt', type: 'TIMESTAMP(3)' },
+        { name: 'updatedAt', type: 'TIMESTAMP(3)' }
+      ]
+    },
+    {
+      moduleDetails: { name: 'USERS' },
+      columnDetails: [
+        { name: 'id', type: 'UUID' },
+        { name: 'role_id', type: 'UUID' },
+        { name: 'first_name', type: 'VARCHAR(50)' },
+        { name: 'last_name', type: 'VARCHAR(50)' },
+        { name: 'email', type: 'VARCHAR(256)' },
+        { name: 'password', type: 'VARCHAR(128)' },
+        { name: 'createdAt', type: 'TIMESTAMP(3)' },
+        { name: 'updatedAt', type: 'TIMESTAMP(3)' }
+      ]
+    },
+    {
+      moduleDetails: { name: 'MODULES' },
+      columnDetails: [
+        { name: 'id', type: 'UUID' },
+        { name: 'name', type: 'VARCHAR(50)' },
+        { name: 'icon', type: 'TEXT' },
+        { name: 'isPrivate', type: 'TEXT' },
+        { name: 'createdAt', type: 'TIMESTAMP(3)' },
+        { name: 'updatedAt', type: 'TIMESTAMP(3)' }
+      ]
+    }
+  ];
+  modules.map(({ moduleDetails, columnDetails }) =>
+    ModuleService.createOne(moduleDetails, columnDetails)
   );
   await Promise.all(modules);
 };
