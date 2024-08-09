b8cbf4a6b9a8aca9a2e564c0d78be8fe5223320d
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-08-09 12:17:31 +0530

    Application: Minor bug fix

--- apps/auth-server/src/models/schema/index.ts
+++ apps/auth-server/src/models/schema/index.ts
@@ -1,1 +1,8 @@
+import { UserSelect } from '@fullstack-package/auth-schemas';
+declare global {
+  namespace Express {
+    interface User extends UserSelect {}
+  }
+}
+
 export * from '@fullstack-package/auth-schemas';
--- apps/dashboard-server/src/models/schema/index.ts
+++ apps/dashboard-server/src/models/schema/index.ts
@@ -1,1 +1,9 @@
+import { DetailedUser } from '@fullstack-package/dashboard-schemas';
+
+declare global {
+  namespace Express {
+    interface User extends DetailedUser {}
+  }
+}
+
 export * from '@fullstack-package/dashboard-schemas';
