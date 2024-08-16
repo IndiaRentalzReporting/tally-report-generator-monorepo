828e8eec68e4653b59d070743024d848acdba266
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-08-16 21:28:46 +0530

    Utils: Init

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/loaders/index.ts', 'src/middlewares/index.ts'],
+  entry: ['src/index.ts'],
   dts: true,
   format: ['esm', 'cjs'],
   clean: true,
