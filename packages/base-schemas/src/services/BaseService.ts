9d8affe1122ad0e32e045e4a96ac00c4ca621794
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-08-16 14:12:52 +0530

    Base-Schema: Shifted base schema service and create client func

---
+++ packages/base-schemas/src/services/BaseService.ts
@@ -0,0 +1,137 @@
+import { and, eq, TableRelationalConfig } from 'drizzle-orm';
+import { PgTableWithColumns } from 'drizzle-orm/pg-core';
+import { RelationalQueryBuilder } from 'drizzle-orm/pg-core/query-builders/query';
+import { NotFoundError } from '@fullstack_package/core-application/errors';
+import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+export class BaseService<
+  H extends Record<string, unknown>,
+  T extends PgTableWithColumns<{
+    name: string;
+    schema: undefined;
+    columns: Record<string, any>;
+    dialect: 'pg';
+  }>,
+  K extends RelationalQueryBuilder<T, TableRelationalConfig>
+> {
+  private entity: string;
+  constructor(
+    protected dbClient: PostgresJsDatabase<H>,
+    protected schema: T,
+    protected tableName: K
+  ) {
+    //@ts-ignore
+    this.entity = this.tableName.tableConfig.dbName;
+  }
+
+  public static createClient<T extends Record<string, unknown>>(
+    URL: string,
+    schema: T,
+    options: {
+      DB_MIGRATING: boolean;
+      DB_SEEDING: boolean;
+    }
+  ): { db: PostgresJsDatabase<T>; connection: postgres.Sql } {
+    try {
+      const connection = postgres(URL, {
+        max: options.DB_MIGRATING || options.DB_SEEDING ? 1 : undefined
+      });
+      return {
+        db: drizzle(connection, {
+          schema,
+          logger: true
+        }),
+        connection
+      };
+    } catch (e) {
+      console.error(`Could not create Database client: ${e}`);
+      throw e;
+    }
+  }
+
+  public async createOne(data: T['$inferInsert']): Promise<T['$inferSelect']> {
+    const [entity] = await this.dbClient
+      .insert(this.schema)
+      .values(data)
+      .returning();
+
+    if (!entity)
+      throw new NotFoundError(`${this.entity} returned as undefined`);
+
+    return entity;
+  }
+
+  public async findMany(
+    data: Partial<T['$inferSelect']> = {},
+    extra?: Omit<NonNullable<Parameters<K['findMany']>[0]>, 'where'>
+  ): Promise<NonNullable<Awaited<ReturnType<K['findMany']>>>> {
+    const keys = Object.keys(data) as Array<
+      keyof Partial<typeof this.schema.$inferSelect>
+    >;
+    const values = Object.values(data) as Array<any>;
+    const entity = await this.tableName.findMany({
+      where: and(
+        ...keys.map((key, index) => eq(this.schema[key], values[index]))
+      ),
+      ...extra
+    });
+
+    if (!entity.length) {
+      throw new NotFoundError(`${this.entity} does not exist`);
+    }
+
+    return entity as NonNullable<Awaited<ReturnType<K['findMany']>>>;
+  }
+
+  public async findOne(
+    data: Partial<T['$inferSelect']>,
+    extra?: Omit<NonNullable<Parameters<K['findFirst']>[0]>, 'where'>
+  ): Promise<NonNullable<Awaited<ReturnType<K['findFirst']>>>> {
+    const keys = Object.keys(data) as Array<
+      keyof Partial<typeof this.schema.$inferSelect>
+    >;
+    const values = Object.values(data) as Array<any>;
+
+    const entity = await this.tableName.findFirst({
+      where: and(
+        ...keys.map((key, index) => eq(this.schema[key], values[index]))
+      ),
+      ...extra
+    });
+
+    if (!entity) {
+      throw new NotFoundError(`${this.entity} does not exist`);
+    }
+
+    return entity as NonNullable<Awaited<ReturnType<K['findFirst']>>>;
+  }
+
+  public async updateOne(
+    id: T['$inferSelect']['id'],
+    data: Partial<T['$inferInsert']>
+  ): Promise<T['$inferSelect']> {
+    const [entity] = await this.dbClient
+      .update(this.schema)
+      .set({ ...data })
+      .where(eq(this.schema.id, id))
+      .returning();
+
+    if (!entity) throw new NotFoundError(`${this.entity} does not exits`);
+
+    return entity;
+  }
+
+  public async deleteOneById(
+    id: T['$inferSelect']['id']
+  ): Promise<T['$inferSelect']> {
+    const [entity] = await this.dbClient
+      .delete(this.schema)
+      .where(eq(this.schema.id, id))
+      .returning();
+
+    if (!entity) throw new NotFoundError(`${this.entity} does not exits`);
+
+    return entity;
+  }
+}
