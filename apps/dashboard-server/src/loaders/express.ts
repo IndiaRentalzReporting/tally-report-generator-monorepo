2863e31aae7da78a397fe8de8abb7fd30bcb2a80
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-08-21 01:57:38 +0530

    Loaders: Renamed to Express

--- apps/dashboard-server/src/loaders/express.ts
+++ apps/dashboard-server/src/loaders/express.ts
@@ -2,7 +2,7 @@ import 'express-async-errors';
 import cors from 'cors';
 import { Express } from 'express';
 import routesLoader from './routes';
-import { expressLoader } from '@trg_package/loaders';
+import { expressLoader } from '@trg_package/express';
 
 const appLoader = async (): Promise<Express> => {
   const app = await expressLoader(routesLoader);
