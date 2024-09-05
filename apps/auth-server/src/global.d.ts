7d2ae826fb663c86b86f6c3154f65b54773395c9
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-09-06 00:01:27 +0530

    Server: Declarration file in index.ts only

--- apps/auth-server/src/global.d.ts
+++
@@ -1,8 +0,0 @@
-import { DetailedUser as AuthDetailedUser } from '@trg_package/auth-schemas/types';
-import { DetailedUser as DashDetailedUser } from '@trg_package/dashboard-schemas/types';
-
-declare global {
-  namespace Express {
-    interface User extends AuthDetailedUser, DashDetailedUser {}
-  }
-}
--- apps/dashboard-server/src/index.ts
+++ apps/dashboard-server/src/index.ts
@@ -1,5 +1,18 @@
 import expressLoader from './loaders/express';
 import config from './config';
+import type { DetailedUser as AuthDetailedUser } from '@trg_package/auth-schemas/types';
+import type { DetailedUser as DashDetailedUser } from '@trg_package/dashboard-schemas/types';
+import * as dashboardSchemas from '@trg_package/dashboard-schemas/schemas';
+import type {
+  UserService,
+  RoleService,
+  ModuleService,
+  ActionService,
+  PermissionService,
+  ApiKeyService
+} from '@trg_package/dashboard-schemas/services';
+import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import type { Sql } from 'postgres';
 
 const { PORT, NODE_ENV } = config;
 
@@ -15,3 +28,21 @@ const { PORT, NODE_ENV } = config;
     console.error('Could not connect to the server');
   }
 })();
+
+declare global {
+  namespace Express {
+    interface Request {
+      module?: string;
+      action?: string;
+      userService: UserService;
+      roleService: RoleService;
+      moduleService: ModuleService;
+      actionService: ActionService;
+      permissionService: PermissionService;
+      apiKeyService: ApiKeyService;
+      dashboardDb: PostgresJsDatabase<typeof dashboardSchemas>;
+      dashboardConnection: Sql<{}>;
+    }
+    interface User extends AuthDetailedUser, DashDetailedUser {}
+  }
+}
