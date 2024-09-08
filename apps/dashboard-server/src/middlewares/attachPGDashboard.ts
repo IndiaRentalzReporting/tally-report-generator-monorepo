748216f82a5eda6ef219dfb3d4927f32094b37bb
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-09-08 19:35:59 +0530

    Dashboard-Server: Bug fixed for session Optimization left

--- apps/dashboard-server/src/middlewares/attachPGDashboard.ts
+++ apps/dashboard-server/src/middlewares/attachPGDashboard.ts
@@ -1,4 +1,4 @@
-import { BadRequestError } from '@trg_package/errors';
+import { BadRequestError, UnauthenticatedError } from '@trg_package/errors';
 import { NextFunction, Response, Request } from 'express';
 import * as dashboardSchemas from '@trg_package/dashboard-schemas/schemas';
 import { createUrl, createClient } from '@trg_package/create-pg-client';
@@ -8,27 +8,32 @@ export const attachPGDashboard = async (
   res: Response,
   next: NextFunction
 ) => {
-  if (req.isAuthenticated() && req.user.tenant_id) {
-    const { db_name, db_username, db_password } = req.user.tenant;
+  try {
+    if (!!req.user) {
+      const { db_name, db_username, db_password } = req.user.tenant;
 
-    if (!db_name || !db_username || !db_password) {
-      throw new BadRequestError('Tenant database error, missing credentials');
-    }
+      if (!db_name || !db_username || !db_password) {
+        throw new BadRequestError('Tenant database error, missing credentials');
+      }
 
-    const DASHBOARD_PG_URL = createUrl({
-      db_username,
-      db_password,
-      db_name
-    });
-    const { client: dashboardDb, connection: dashboardConnection } =
-      createClient(DASHBOARD_PG_URL, dashboardSchemas, {
-        DB_MIGRATING: false,
-        DB_SEEDING: false
+      const DASHBOARD_PG_URL = createUrl({
+        db_username,
+        db_password,
+        db_name
       });
+      const { client: dashboardDb, connection: dashboardConnection } =
+        createClient(DASHBOARD_PG_URL, dashboardSchemas, {
+          DB_MIGRATING: false,
+          DB_SEEDING: false
+        });
 
-    req.dashboardDb = dashboardDb;
-    req.dashboardConnection = dashboardConnection;
+      req.dashboardDb = dashboardDb;
+      req.dashboardConnection = dashboardConnection;
+      next();
+    } else {
+      throw new UnauthenticatedError('User not found!');
+    }
+  } catch (e) {
+    return next(e);
   }
-
-  next();
 };
--- apps/dashboard-server/src/middlewares/index.ts
+++ apps/dashboard-server/src/middlewares/index.ts
@@ -1,2 +1,5 @@
 export * from './attachModuleActionData';
+export * from './attachServices';
+export * from './attachPGDashboard';
+export * from './attachUser';
 export * from './isRoleAllowed';
