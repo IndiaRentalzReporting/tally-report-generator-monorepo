3799c4deaaa5a5959b2030e540688c689af90128
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-09-15 14:35:15 +0530

    AxiosClient: Little optimization and refactoring

--- packages/axios-client/src/config/index.ts
+++ packages/axios-client/src/config/index.ts
@@ -3,7 +3,6 @@ import z from 'zod';
 
 const EnvSchema = z.object({
   VITE_NODE_ENV: z.enum(['production', 'development', 'staging']),
-  VITE_PORT: z.coerce.number(),
   VITE_DOMAIN: z.string(),
   VITE_TLD: z.string(),
   VITE_AUTH_SUBDOMAIN: z.string(),
