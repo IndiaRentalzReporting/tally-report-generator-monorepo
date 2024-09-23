4bb20e7338d939d0d162e1e5623f6ba72bdddbf3
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-09-24 02:20:12 +0530

    SchemasBase: minor change
    Converted into a function for curbing the unique key constraint bug

--- packages/schemas-base/src/schemas/base.ts
+++ packages/schemas-base/src/schemas/base.ts
@@ -7,20 +7,20 @@ export const EntityStatus = pgEnum('status', [
   'inactive'
 ]);
 
-export const EntityTimestamps = {
+export const EntityTimestamps = () => ({
   createdAt: timestamp('createdAt').notNull().defaultNow(),
   updatedAt: timestamp('updatedAt')
     .defaultNow()
     .$onUpdate(() => new Date()),
   deletedAt: timestamp('deletedAt'),
   approvedAt: timestamp('approvedAt')
-};
+});
 
-export const BaseEntitySchema = {
+export const BaseEntitySchema = () => ({
   id: uuid('id').primaryKey().notNull().defaultRandom(),
   name: varchar('name', { length: 200 }).notNull(),
   status: EntityStatus('status').default('active').notNull(),
   isReadonly: boolean('isReadonly').notNull().default(false),
   isPrivate: boolean('isPrivate').notNull().default(false),
-  ...EntityTimestamps
-};
+  ...EntityTimestamps()
+});
