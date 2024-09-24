55854723a63f08b052bc44e76f3fc263e56abb1e
Hargun Singh <hargun0112@gmail.com>
2024-09-24 18:27:25 +0530

    adding types exports

---
+++ packages/schemas-reporting/src/types/Column.ts
@@ -0,0 +1,15 @@
+import { ColumnTypeLiteral } from "./ColumnTypes"
+
+export {
+    type ColumnInsert,
+    ColumnInsertSchema
+} from "../schemas/column"
+
+export type ColumnSelect = {
+    name : string,
+    table : string,
+    tablealias : string,
+    type : ColumnTypeLiteral
+}
+
+//ColumnSelectSchema not required
