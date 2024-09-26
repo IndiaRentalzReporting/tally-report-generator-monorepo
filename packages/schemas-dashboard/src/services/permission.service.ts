d384eaf7594b5ded64e9b21a6d2c733df8fb5d16
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-09-26 14:56:53 +0530

    DashboardSchemas: Removed assignaction method from permission service

--- packages/schemas-dashboard/src/services/permission.service.ts
+++ packages/schemas-dashboard/src/services/permission.service.ts
@@ -2,27 +2,13 @@ import { BaseServiceNew } from '@trg_package/base-service';
 import { PermissionSchema } from '../schemas';
 import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import * as dashboardSchemas from '../schemas';
-import { PermissionActionService } from './permission_action.service';
 
 export class PermissionService extends BaseServiceNew<
   typeof dashboardSchemas,
   typeof PermissionSchema
 > {
-  private PermissionActionService: PermissionActionService;
-
   constructor(db: PostgresJsDatabase<typeof dashboardSchemas>) {
     super(db, PermissionSchema, db.query.PermissionSchema);
-    this.PermissionActionService = new PermissionActionService(db);
-  }
-
-  public async assignAction({
-    permission_id,
-    action_id
-  }: {
-    permission_id: string;
-    action_id: string;
-  }) {
-    await this.PermissionActionService.createOne({ permission_id, action_id });
   }
 
   public async findMany(data: Partial<typeof this.schema.$inferSelect> = {}) {
