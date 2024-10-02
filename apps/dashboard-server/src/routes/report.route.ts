8273f2ba2cb653960e874e958834867aeee3610c
Hargun Singh <hargun0112@gmail.com>
2024-10-03 00:58:35 +0530

    add routes to report router

--- apps/dashboard-server/src/routes/report.route.ts
+++ apps/dashboard-server/src/routes/report.route.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { validateSchema } from '@trg_package/middlewares';
-import { createOne } from '../controller/report.controller';
-import { ReportInsertSchema } from '@trg_package/schemas-reporting/types';
+import { createOne, deleteOne, readAll, updateOne } from '../controller/report.controller';
+import { ReportInsertSchema, ReportSelectSchema } from '@trg_package/schemas-reporting/types';
 
 
 const reportRouter = Router();
@@ -20,5 +20,37 @@ reportRouter.post(
     createOne
 );
 
+reportRouter.get(
+    '/read',
+    validateSchema({
+      query: ReportSelectSchema.pick({
+        id : true
+      })
+    }),
+    readAll
+);
+
+
+
+reportRouter.patch(
+    '/update/:id',
+    validateSchema({
+      body: ReportInsertSchema,
+      params: ReportSelectSchema.pick({
+        id: true
+      })
+    }),
+    updateOne
+);
+
+reportRouter.delete(
+    '/delete/:id',
+    validateSchema({
+        params: ReportSelectSchema.pick({
+        id: true
+        })
+    }),
+    deleteOne
+);
 
 export default reportRouter;
