1aeda19b7658325a929e26520b71681ea33a721a
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-10-05 16:30:44 +0530

    Components: Removed and added to vite package

--- packages/components/src/components/ui/loadingSpinner.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Loader2 } from 'lucide-react';
-
-export const LoadingSpinner: React.FC<{ className?: string }> = ({
-  className = '',
-}) => (
-  <Loader2 className={`w-8 h-8 animate-spin invert-0 mx-auto ${className}`} />
-);
