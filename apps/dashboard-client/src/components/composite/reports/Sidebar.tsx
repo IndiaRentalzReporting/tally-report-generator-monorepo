c3c6a47f57a6d09abd7d950c4292f89b86615f49
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-10-08 02:06:53 +0530

    DashboardClient: Minor ui changes

--- apps/dashboard-client/src/components/composite/reports/Sidebar.tsx
+++ apps/dashboard-client/src/components/composite/reports/Sidebar.tsx
@@ -7,12 +7,12 @@ const Sidebar: React.FC = () => {
 
   return (
     <div className="hidden border-r bg-muted/40 md:block">
-      <div className="flex h-full max-h-screen flex-col gap-2">
+      <div className="flex h-full max-h-screen flex-col">
         <div className="flex items-center justify-center h-14 border-b px-4 lg:h-[60px] lg:px-6">
           <h2 className="text-xl font-bold">Available Columns</h2>
         </div>
         <div className="flex-1">
-          <nav className="grid items-start px-2 text-sm font-medium lg:px-4 gap-2">
+          <nav className="grid items-start px-2 pt-6 text-sm font-medium lg:px-4 gap-2">
             {availableColumns.map((col) => (
               <Button
                 className="w-full"
