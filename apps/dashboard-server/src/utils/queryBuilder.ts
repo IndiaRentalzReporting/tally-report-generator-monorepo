97b4dfa5e0bfa1dcab14413d59b3783eb1f81e7a
Hargun Singh <hargun0112@gmail.com>
2024-10-19 18:08:55 +0530

    dashboard-sever : store filters in queryConfig as required

--- apps/dashboard-server/src/utils/queryBuilder.ts
+++ apps/dashboard-server/src/utils/queryBuilder.ts
@@ -1,5 +1,10 @@
 import { ReportInsert, ReportSelect } from '@trg_package/schemas-reporting/types';
 
+/**
+ *
+ * @param param : any
+ * @returns Value with the required quotes
+ */
 function getEscapedValue(param : any) {
   if (Array.isArray(param)) {
     param.map((e) => getEscapedValue(e));
@@ -12,12 +17,18 @@ function getEscapedValue(param : any) {
   return `'${param}'`;
 }
 
+/**
+ *
+ * @param column
+ * @returns The tablename.columnname together for the query
+ */
 function getColumnName(column : NonNullable<ReportInsert['columns']>[number]['column']) {
   return `${column.tablealias}."${column.name}"`;
 }
+
 export function getColumnQuery(columns : ReportInsert['columns']) {
   const query: string[] = [];
-  columns?.map((ele) => {
+  columns?.forEach((ele) => {
     const { column } = ele;
     if (ele.operation) query.push(`${ele.operation}(${getColumnName(column)}) as "${column.alias}"`);
     else query.push(`${getColumnName(column)} as \\"${column.alias}\\"`);
@@ -27,7 +38,7 @@ export function getColumnQuery(columns : ReportInsert['columns']) {
 
 export function getConditionQuery(conditions : ReportInsert['conditions']) {
   const query : string[] = [];
-  conditions?.map((condition) => {
+  conditions?.forEach((condition) => {
     if (condition.params === null) query.push(`${condition.join} ${condition.operator}(${getColumnName(condition.column)})`);
     else if (condition.params && 'value' in condition.params) query.push(`${condition.join} ${getColumnName(condition.column)} ${condition.operator} ${getEscapedValue(condition.params.value)}`);
     else if (condition.params && 'from' in condition.params && 'to' in condition.params) query.push(`${condition.join} ${getColumnName(condition.column)} ${condition.operator} ${getEscapedValue(condition.params.from)} AND ${getEscapedValue(condition.params.to)}`);
@@ -37,13 +48,26 @@ export function getConditionQuery(conditions : ReportInsert['conditions']) {
 }
 export function getGroupByQuery(columns : ReportSelect['groupBy']) {
   const query : string[] = [];
-  columns?.map((column) => {
+  columns?.forEach((column) => {
     query.push(`${getColumnName(column.column)}`);
   });
 
   return `GROUP BY ${query.join(', ')}`;
 }
 
+type FilterConfig = NonNullable<ReportInsert['queryConfig']>['filters'];
+export function getFilterConfig(filters : NonNullable<ReportInsert['filters']>) : FilterConfig {
+  const filterConfig : FilterConfig = [];
+  filters.forEach((e) => {
+    let dataSource = null;
+    if (e.filterType === 'select') {
+      dataSource = `SELECT ${getColumnName(e.column)} as label, as value FROM public."${e.column.table}"`;
+    }
+    filterConfig.push({ ...e,dataSource });
+  });
+
+  return filterConfig;
+}
 export function getQueryConfig(tableQuery : string, report : ReportInsert) : ReportInsert['queryConfig'] {
   const {
     columns,conditions,groupBy,filters
@@ -52,6 +76,7 @@ export function getQueryConfig(tableQuery : string, report : ReportInsert) : Rep
   const columnQuery = columns ? getColumnQuery(columns) : ' * ';
   const condtionsQuery = conditions ? getConditionQuery(conditions) : ' ';
   const groupByQuery = groupBy ? getGroupByQuery(groupBy) : ' ';
+  const filterConfig = filters ? getFilterConfig(filters) : null;
 
   const query = `SELECT ${columnQuery} FROM ${tableQuery} ${condtionsQuery} ${groupByQuery}`;
 
@@ -63,6 +88,6 @@ export function getQueryConfig(tableQuery : string, report : ReportInsert) : Rep
   return {
     dataSource: query,
     columns: columnArr,
-    filters: []
+    filters: filterConfig
   };
 }
