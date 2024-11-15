0ce087a8f7d03a0019d5c1ddc773737f69bc1e4c
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-11-15 13:40:42 +0530

    DashboardServer: Minor change

--- apps/dashboard-server/src/middlewares/isRoleAllowed.ts
+++ apps/dashboard-server/src/middlewares/isRoleAllowed.ts
@@ -18,9 +18,6 @@ export const isRoleAllowed = async (
         'You are not allowed to do anything, please get a role assigned to yourself'
       );
     }
-    if (role.name == 'SUPERUSER') {
-      return next();
-    }
 
     if (module && action) {
       const { permission } = role;
