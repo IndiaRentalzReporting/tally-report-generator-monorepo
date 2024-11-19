160fbcd7de84aa4bd72c3012d7285211b203a83c
hargun singh <hargun0112@gmail.com>
2024-11-19 22:05:55 +0530

    seperate read routes in a readRouter for reporting

--- apps/dashboard-server/src/routes/report.route.ts
+++ apps/dashboard-server/src/routes/report.route.ts
@@ -1,25 +1,15 @@
 import { Router } from 'express';
 import { validateSchema } from '@trg_package/middlewares';
 import {
-  ColumnInsertSchema,
   ReportSelectSchema,
-  TableSelectSchema,
-  ReportInsertSchema,
-  FilterValueSchema
+  ReportInsertSchema
 } from '@trg_package/schemas-reporting/types';
-import z from 'zod';
 import {
   createOne,
-  deleteOne,
-  getAllColumns,
-  getAllTables,
-  getReportColumns,
-  getReportData,
-  getReportFilters,
-  getSelectData,
-  readAll
+  deleteOne
 } from '../controller/report.controller';
 import reportUpdateRouter from './reportUpdate.route';
+import reportReadRouter from './reportRead.route';
 
 const reportRouter = Router();
 
@@ -35,14 +25,7 @@ reportRouter.post(
   createOne
 );
 
-reportRouter.get(
-  '/read',
-  validateSchema({
-    query: ReportSelectSchema.partial()
-  }),
-  readAll
-);
-
+reportRouter.use('/read',reportReadRouter);
 reportRouter.use('/update',reportUpdateRouter);
 
 reportRouter.delete(
@@ -53,87 +36,6 @@ reportRouter.delete(
     })
   }),
   deleteOne
-);
-
-reportRouter.get(
-  '/read/columns/:tableId',
-  validateSchema({
-    params: z.object({
-      tableId: TableSelectSchema.shape.id
-    })
-  }),
-  getAllColumns
-);
-
-reportRouter.get(
-  '/read/tables',
-  getAllTables,
-);
-
-// report level access pending
-reportRouter.get(
-  '/read/selectData/:id',
-  validateSchema({
-    params: ColumnInsertSchema.pick({
-      id: true
-    })
-  }),
-  getSelectData
-);
-
-reportRouter.get(
-  '/read/reportColumns/:id',
-  validateSchema({
-    params: ReportInsertSchema.pick({
-      id: true
-    })
-  }),
-  getReportColumns
-);
-
-reportRouter.get(
-  '/read/reportData/:id',
-  validateSchema({
-    params: ReportInsertSchema.pick({
-      id: true
-    }),
-    query: z.object({
-      pageSize: z.coerce.number(),
-      pageIndex: z.coerce.number(),
-      filters: z.optional(
-        z.string().refine((val) => {
-          try {
-            const parsed = JSON.parse(val);
-            return typeof parsed === 'object' && !Array.isArray(parsed);
-          } catch {
-            return false;
-          }
-        }, {
-          message: 'filters must be a valid JSON object',
-        })
-          .transform((val) => JSON.parse(val))
-          .refine(
-            (filters) => Object
-              .values(filters)
-              .every((item) => FilterValueSchema.safeParse(item).success),
-            {
-              message: 'Each filter must be of type { value: string | string[] } or { from: string, to: string }',
-            }
-          )
-      )
-    }).strict()
-  }),
-  getReportData
-);
-
-reportRouter.get(
-  '/read/reportFilters/:id',
-  validateSchema({
-    params: ReportInsertSchema.pick({
-      id: true
-    })
-  }),
-  getReportFilters
 );
 
 export default reportRouter;
