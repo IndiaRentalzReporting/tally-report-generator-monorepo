94a54f68cc3e833ff6382fb964135266450d4f6d
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-11-22 22:21:42 +0530

    AuthClient: New layout

--- apps/auth-client/src/App.tsx
+++ apps/auth-client/src/App.tsx
@@ -10,20 +10,17 @@ import { SignUpForm } from '@/views/Authentication/Signup';
 import { SignInForm } from '@/views/Authentication/Signin';
 import { ForgotPassword } from '@/views/Authentication/ForgotPassword';
 import { ResetPassword } from '@/views/Authentication/ResetPassword';
-import { RootLayout } from './components/composite';
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements([
-      <Route path="/" element={<RootLayout />}>
-        <Route element={<PublicRoutes/>}>
-          <Route index element={<Navigate to="sign-up" />} />
-          <Route path="sign-up" element={<SignUpForm />} />
-          <Route path="sign-in" element={<SignInForm />} />
-          <Route path="forgot-password" element={<ForgotPassword />} />
-          <Route path="reset-password/:token" element={<ResetPassword />} />
-          <Route path="*" element={<Navigate to="sign-up" />} />
-        </Route>
+      <Route path='/' element={<PublicRoutes/>}>
+        <Route index element={<Navigate to="sign-up" />} />
+        <Route path="sign-up" element={<SignUpForm />} />
+        <Route path="sign-in" element={<SignInForm />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="reset-password/:token" element={<ResetPassword />} />
+        <Route path="*" element={<Navigate to="sign-up" />} />
       </Route>
     ])
   );
