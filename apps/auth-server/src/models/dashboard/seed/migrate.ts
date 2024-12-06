0ff69b14452e22cbdb2df5ff071571cff850026d
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-12-06 13:54:08 +0530

    migration changes

--- apps/auth-server/src/models/dashboard/seed/migrate.ts
+++ apps/auth-server/src/models/dashboard/seed/migrate.ts
@@ -1,15 +1,19 @@
 import { execSync } from 'child_process';
-import { DASHBOARD_PG_URL } from '../../../config';
+import path from 'path';
+import config, { DASHBOARD_PG_URL } from '../../../config';
 
+const { NODE_ENV } = config;
 export function migrateDashboardSchema(PG_URL: string) {
+  const appDir = path.resolve(__dirname, '../../../../../../');
   const envVariable = `${DASHBOARD_PG_URL}=${PG_URL}`;
-  const scriptName = 'db-dashboard:migrate';
+  const scriptName = `db-dashboard:migrate:${NODE_ENV}`;
 
   try {
     execSync(
       `${envVariable} npm run ${scriptName}`,
       {
-        stdio: 'inherit'
+        stdio: 'inherit',
+        cwd: appDir
       }
     );
     console.log('\n Migration Completed Successfully');
